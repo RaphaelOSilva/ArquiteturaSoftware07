4a221a9de74133b81add395106d342a441134e9f
RaphaelOSilva <36748813+RaphaelOSilva@users.noreply.github.com>
2018-05-08 19:52:51 -0300

    Initial Commit

---
+++ Exercices_Patterns-master/Exercice_Pattern/Exercicio2/SingletonExercice.java
@@ -0,0 +1,16 @@
+package Exercicio2;
+
+/**
+ * 
+ * @author Raphael Silva - 816110743
+ *
+ */
+
+public class SingletonExercice {
+	public static void main(String args[]) {
+		Deck deck = new Deck( );
+		
+		deck.print( );
+		}
+
+}
